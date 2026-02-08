2a1647a339ea55d632aac98766b9d65300b1e980
Elham-EN <ilhamrezaie@gmail.com>
2026-02-08 13:01:10 +1100

    created ios 26 tinted glass native bar

--- src/app/share.tsx
+++ src/app/share.tsx
@@ -1,9 +1,9 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+export default function Tab() {
   return (
     <View style={styles.container}>
-      <Text>Inbox Screen</Text>
+      <Text>Share Screen</Text>
     </View>
   );
 }
