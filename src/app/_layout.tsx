d7835304d82762f099ed7b3d75b1bccca20708e2
Elham-EN <ilhamrezaie@gmail.com>
2026-02-08 20:47:55 +1100

    improvenmnt on route organization

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,28 +1,10 @@
-import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
+/**
+ * a simple Stack — this becomes the top-level navigator
+ * that will eventually switch between groups
+ */
 
-export default function TabLayout() {
-  return (
-    <NativeTabs>
-      <NativeTabs.Trigger name="index">
-        <Label>Inbox</Label>
-        <Icon sf={"tray.and.arrow.down"} drawable="custom_android_drawable" />
-      </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="today">
-        <Icon sf="checkmark.app" drawable="custom_settings_drawable" />
-        <Label>Today</Label>
-      </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="calendar">
-        <Icon sf="calendar" drawable="custom_settings_drawable" />
-        <Label>Calendar</Label>
-      </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="share">
-        <Icon sf="person.2" drawable="custom_settings_drawable" />
-        <Label>Sharing</Label>
-      </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="profile">
-        <Icon sf="person.crop.circle" drawable="custom_settings_drawable" />
-        <Label>Profile</Label>
-      </NativeTabs.Trigger>
-    </NativeTabs>
-  );
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
